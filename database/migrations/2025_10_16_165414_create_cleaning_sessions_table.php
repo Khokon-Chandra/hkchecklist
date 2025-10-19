274f998e298e45ed6bf949b8ffd6c42129412a48
Khokon-Chandra <khokonchandra4@gmail.com>
2025-10-19 14:53:05 +0600

    added unique key cs_hkp_prop_sched_uq

--- database/migrations/2025_10_16_165414_create_cleaning_sessions_table.php
+++ database/migrations/2025_10_16_165414_create_cleaning_sessions_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->decimal('start_latitude', 10, 7)->nullable();
             $table->decimal('start_longitude', 10, 7)->nullable();
             $table->timestamps();
-            $table->unique(['housekeeper_id', 'property_id', 'scheduled_date']);
+            $table->unique(['housekeeper_id', 'property_id', 'scheduled_date'], 'cs_hkp_prop_sched_uq');
         });
     }
 
