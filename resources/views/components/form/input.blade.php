472e6ab1e14578fb51f3dcdda6f22a5ed7026e3c
Khokon-Chandra <khokonchandra4@gmail.com>
2026-01-09 10:36:47 +0600

    Implement theme color from settings in input component

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -4,13 +4,24 @@
 ])
 
 @php
+    use App\Models\Setting;
+
     $withiconClasses = $withicon ? 'pl-11 pr-4' : 'px-4';
+    $themeColor = Setting::get('theme_color', '#842eb8');
 @endphp
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
-    'class' =>
-        $withiconClasses .
-        ' py-1 border-gray-300 rounded-md focus:border-gray-500 focus:ring
-            focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-white dark:border-gray-700 dark:bg-dark-eval-1
-            dark:text-gray-300 dark:focus:ring-offset-dark-eval-1',
-]) !!}>
+<input {{ $disabled ? 'disabled' : '' }}
+       data-focus-ring="{{ $themeColor }}"
+       {!! $attributes->merge([
+           'class' =>
+               $withiconClasses .
+               ' py-1 border-gray-300 rounded-md focus:border-gray-500 focus:ring
+                   focus:ring-offset-2 focus:ring-offset-white dark:border-gray-700 dark:bg-dark-eval-1
+                   dark:text-gray-300 dark:focus:ring-offset-dark-eval-1',
+       ]) !!}>
+
+<style>
+    [data-focus-ring="{{ $themeColor }}"]:focus {
+        --tw-ring-color: {{ $themeColor }} !important;
+    }
+</style>
